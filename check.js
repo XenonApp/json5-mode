a7d57ecc78899689443e47f80e775d26d92f8f69
Zef Hemel <zef@zef.me>
2014-04-14 11:40:10 +0200

    Initial check

---
+++ check.js
@@ -0,0 +1,19 @@
+var JSON5 = require("./json5.js");
+
+/**
+ * Required inputs: text
+ */
+module.exports = function(info) {
+    var text = info.inputs.text;
+    try {
+        JSON5.parse(text);
+    } catch (e) {
+        var lines = text.substring(0, e.at).split("\n");
+        return [{
+            row: lines.length - 1,
+            text: e.message,
+            type: "error"
+        }];
+    }
+    return [];
+};
